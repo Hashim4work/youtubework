21ade71b1fb43d8bec9851c6af3a25b5fdf02a1d
Hashim4work <s.hashimali512@gmail.com>
2019-03-26 17:10:51 +0500

    after changing gem files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_082631) do
+ActiveRecord::Schema.define(version: 2019_03_19_135902) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsqx xl"
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
